010fa1222f94fee0c72b8c0a4cf6b947ea3e96e9
Denisa <denisa.gerez@gmail.com>
2023-01-04 23:07:27 -0300

    migrado desde css ==>> scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { Page404Component } from './components/page404/page404.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full'},
   { path: 'inicio', component: HomeComponent },
-  { path: 'contact', component: ContactComponent}
+  { path: 'contact', component: ContactComponent },
+  { path: 'projects', component: ProjectsComponent},
+  { path: '**', component: Page404Component}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { FooterComponent } from './components/shared/footer/footer.component';
     HeaderComponent,
     ContactComponent,
     FooterComponent,
+    NavComponent,
+    ProjectsComponent,
+    Page404Component,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
